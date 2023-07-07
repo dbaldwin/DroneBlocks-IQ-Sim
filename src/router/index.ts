0fa01ea3e48bdf13c5daea59e7e6dd605c0e4add
Dennis Baldwin <dennisbaldwin@gmail.com>
2023-07-07 09:13:50 -0500

    Make map the default view

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
@@ -18,7 +18,7 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/map',
+      path: '/',
       name: 'map',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
